e899d2868dbc6dc1aad532cd68cfd97e88cb58e2
alensouthland <alensouthland@gmail.com>
2025-02-11 22:12:18 +0000

    - edited plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+// export default () => ({});
+
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'local', // Default file upload provider
+        providerOptions: {
+          sizeLimit: 1000000, // Adjust size limit as needed (in bytes)
+        },
+      },
+    },
+  });
