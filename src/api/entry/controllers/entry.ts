1e291d9ba96218749ee85a7ec183e2819b8a96dd
alensouthland <alensouthland@gmail.com>
2025-02-12 16:26:10 +0000

    deleted everything

--- src/api/entry/controllers/entry.ts
+++
@@ -1,7 +0,0 @@
-/**
- * entry controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::entry.entry');
--- src/api/entry/services/entry.ts
+++
@@ -1,7 +0,0 @@
-/**
- * entry service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::entry.entry');
